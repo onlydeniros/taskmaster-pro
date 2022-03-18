71504ac6cdaf1ff5a0a394f3f957e9e8d3d0a81c
DeNiro Dumas <denirod@Deniros-MacBook-Pro.local>
2022-03-18 00:40:14 -0400

    audit and deadline color added

--- assets/js/script.js
+++ assets/js/script.js
@@ -65,9 +65,25 @@ $('#trash').droppable({
 });
 
 
+$('#modalDueDate').datepicker({
+  minDate: 1
+});
 
+var auditTask = function (taskEl) {
+  // get date from task element 
+  var date = $(taskEl).find("span").text().trim();
 
+  var time = moment(date, "L").set("hour", 17);
 
+  $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+
+  if (moment().isAfter(time)) {
+    $(taskEl).addClass("list-group-item-danger");
+  } else if (Math.abs(moment().diff(time, "days")) <= 2) {
+    $(taskEl).addClass("list-group-item-warning");
+  }
+
+}
 
 
 
@@ -89,6 +105,7 @@ var createTask = function (taskText, taskDate, taskList) {
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
 
+  auditTask(taskLi)
 
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
@@ -202,19 +219,23 @@ $(".list-group").on("click", "span", function () {
     .text()
     .trim();
 
-  // create new input element
-  var dateInput = $("<input>")
-    .attr("type", "text")
-    .addClass("form-control")
-    .val(date);
-  $(this).replaceWith(dateInput);
+  var dateInput = $('<input>').attr('type', 'text').addClass('form-control').val(date);
+  $(this).replaceWith(dateInput)
+
+  //enable jquery ui datepicker
+  dateInput.datepicker({
+    minDate: 1,
+    onClose: function () {
+      $(this).trigger('change');
+    }
+  });
 
   // automatically bring up the calendar
   dateInput.trigger("focus");
 });
 
 // value of due date was changed
-$(".list-group").on("blur", "input[type='text']", function () {
+$(".list-group").on("change", "input[type='text']", function () {
   var date = $(this).val();
 
   // get status type and position in the list
@@ -235,6 +256,8 @@ $(".list-group").on("blur", "input[type='text']", function () {
     .addClass("badge badge-primary badge-pill")
     .text(date);
   $(this).replaceWith(taskSpan);
+
+  auditTask($(taskSpan).closest(".list-group-item"));
 });
 
 // remove all tasks
